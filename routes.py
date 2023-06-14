5eff7734359aef820c01547c5cec7bb6739aa6d1
Michele de Barros Santos <michele.fenix.17@gmail.com>
2023-06-14 19:47:04 -0300

    inicial

--- routes.py
+++ routes.py
@@ -1,12 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+lista_usuarios = ['Michele', 'Wallace', 'Murilo', 'Karol']
+
 @app.route("/")
-def hello_world():
-    pass
+def home():
+    return render_template("home.html")
+
+@app.route('/usuarios')
+def usuarios():
+    return render_template('usuarios.html', lista_usuarios=lista_usuarios)
+
+@app.route("/contato")
+def contato():
+    return render_template("contato.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/criar-conta")
+def criarConta():
+    return render_template("criar-conta.html")
 
 
-
-
-app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
